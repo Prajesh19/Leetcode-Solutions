c2bbed2aeb193cd41419a8f2a625bc255ee0fc79
Prajesh <prajeshnithish241@gmail.com>
2025-09-13 13:57:38 +0530

    Time: 0 ms (100%), Space: 40.2 MB (82.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1) return 1;
-        int a = 1, b = 1; // a = ways(n-2), b = ways(n-1)
+        int a = 1, b = 1;
         for (int i = 2; i <= n; i++) {
             int temp = a + b;
             a = b;
